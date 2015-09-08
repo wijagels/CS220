e4ccf5f79fe51f590c533c9c3252cad554c2f649
William Jagels <will.jagels@gmail.com>
2015-09-07 22:47:43 -0400

    Vanquish stupid windows line endings

--- projects/1/encode.c
+++ projects/1/encode.c
@@ -27,8 +27,12 @@ int main(int argc, char **argv) {
 
 char * b64encode(char string[],int len) {
 
-	/* Your code to convert the input array of chars to a base64 string goes here */
-
+    /* Your code to convert the input array of chars to a base64 string goes here */
+    for(int k=0;k < len;++k) {
+        for (int i = 0; i < 8; i++) {
+            printf("%d", !!((string[k] << i) & 0x80));
+        }
+    }
 	b64Result[0]=0; // Make sure result is null terminated
 	return b64Result;
 }
